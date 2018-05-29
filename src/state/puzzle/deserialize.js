b47df4d824d37d9a1c3d0a043bddc78368b15655
Nick Fireman <nfireman1@gmail.com>
2018-05-29 11:10:34 -0400

    add matrix serialization algorithm

--- src/state/puzzle/deserialize.js
+++ src/state/puzzle/deserialize.js
@@ -1,4 +1,4 @@
-import { splitEvery } from 'ramda';
+import { flatten, splitEvery } from 'ramda';
 
 const isNumber = c => c >= '1' && c <= '9';
 const letterSpaces = c => c.charCodeAt(0) - 'a'.charCodeAt(0) + 1;
@@ -19,4 +19,23 @@ export const deserialize = puzzleString => {
   }
 
   return splitEvery(9, result);
+};
+
+const nullEntry = nulls => String.fromCharCode('a'.charCodeAt(0) + nulls - 1);
+
+export const serialize = matrix => {
+  const entries = flatten(matrix);
+
+  return entries.reduce(
+    ({ result, nulls }, entry) => {
+      if (entry === null) {
+        return { result, nulls: nulls + 1 };
+      } else if (nulls === 0) {
+        return { result: result + entry, nulls: 0 };
+      } else {
+        return { result: result + nullEntry(nulls) + entry, nulls: 0 };
+      }
+    },
+    { result: '', nulls: 0 }
+  ).result;
 };
